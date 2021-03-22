b2d8d442e1b8da9cb19730426b5abace789c74da
Francois <francois-67420@hotmail.fr>
2021-03-22 19:07:54 +0100

    Correction du bug de traduction dans le selecteur sur la home page

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -19,7 +19,7 @@ export class HomePage implements OnInit {
   public listsRead: Observable<List[]>;
   public listsWrite: Observable<List[]>;
   public selectedType : number =1;
-  public listsTypes : any[]=[]
+  public listsTypes : any[4]=[]
 
 
   constructor(private listService: ListService,
@@ -28,17 +28,8 @@ export class HomePage implements OnInit {
      public authService : AuthentificationService,
      private platform:Platform) {
        this.platform.ready().then(() =>{
-        this.listsTypes= [ 
-        {id:1 , name: this.translate.instant('general.select_list_type.all') },
-        {id:2 , name: this.translate.instant('general.select_list_type.owned')},
-        {id:3 , name: this.translate.instant('general.select_list_type.write')},
-        {id:4 , name: this.translate.instant('general.select_list_type.read')}
-        ]
-       }) 
-       this.translate.get('general.select_list_type.all').subscribe((res: string) => {
-        console.log(res);
-        //=> 'hello world'
-      })
+        this.loadSelector()
+       })
       }
 
   ngOnInit(){
@@ -48,6 +39,30 @@ export class HomePage implements OnInit {
     this.listsRead = this.listService.listsRead
     this.listsWrite = this.listService.listsWrite
    // this.lists = this.listService.getAllListsOfUser()
+  }
+
+  loadSelector(){
+    console.log("LOAD SELECTOR");
+    this.translate.stream('general.select_list_type.all').subscribe( s=> {
+      this.listsTypes[0]={id:1 , name: s}
+      console.log("changement trad");
+    })
+    this.translate.stream('general.select_list_type.owned').subscribe( s=> {
+      this.listsTypes[1]={id:2 , name: s}
+    })
+    this.translate.stream('general.select_list_type.write').subscribe( s=> {
+      this.listsTypes[2]={id:3 , name: s}
+    })
+    this.translate.stream('general.select_list_type.read').subscribe( s=> {
+      this.listsTypes[3]={id:4 , name: s}
+    })
+/*
+    this.listsTypes= [ 
+      {id:1 , name: this.translate.stream('general.select_list_type.all').pipe( n => n) },
+      {id:2 , name: this.translate.stream('general.select_list_type.owned')},
+      {id:3 , name: this.translate.stream('general.select_list_type.write')},
+      {id:4 , name: this.translate.stream('general.select_list_type.read')}
+      ]*/
   }
 
   async openCreateModal(){
